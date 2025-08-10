e3d5243999c13ef00d38d5342454b62ffc6e8307
Dicko Alou <dickoalou04@gmail.com>
2025-08-10 19:26:18 +0000

    add testing.html

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -1,5 +1,5 @@
 #import
-from flask import Flask
+from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def index():
-    return "testing 123"
+    return render_template("index.html")
 
 if __name__ in "__main__":
     app.run(debug=True)
