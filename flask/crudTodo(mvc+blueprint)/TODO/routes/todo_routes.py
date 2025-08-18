d504b216f37a5e32b1e2c5d668f3ea327d60df5a
Dicko Alou <dickoalou04@gmail.com>
2025-08-18 14:30:04 +0000

    fin du crud todo_list

--- flask/crudTodo(mvc+blueprint)/TODO/routes/todo_routes.py
+++ flask/crudTodo(mvc+blueprint)/TODO/routes/todo_routes.py
@@ -1,11 +1,13 @@
-from flask import Blueprint, redirect, render_template, request
+from flask import Blueprint, redirect, render_template, request, url_for
 from models.todo import todo
 
-action = Blueprint("action", __name__, url_prefix='/todos',static_folder="../static", template_folder="../templates")
+action = Blueprint("action", __name__, static_folder="../static", template_folder="../templates")
 
-@action.route('/')
+@action.route('/', methods=['GET'])
 def index():
-    return render_template("index.html")
+    listofall = todo.get_all()
+    tododict = [t.to_dict() for t in listofall]
+    return render_template("todo_list.html", todos = tododict)
 
 @action.route('/create', methods=['GET', 'POST'])
 def create():
@@ -14,6 +16,31 @@ def create():
 
     title = request.form['title']
     content = request.form['content']
-    status = bool(request.form.get('status', False))
+    status = request.form.get('status', 'pending')
     todo.create(title, content, status)
-    return 'aded'
+    return redirect(url_for('action.index'))
+
+@action.route('/remove', methods = ['GET', 'POST'])
+def remove():
+    listofall = todo.get_all()
+    tododict = [t.to_dict() for t in listofall]
+    if request.method == "GET":
+        return render_template('remove.html', todoid = tododict)
+    else:
+        id = int(request.form['id'])
+        todo.delete(id)
+        return redirect(url_for('action.index'))
+    
+@action.route('/edit', methods = ['GET', 'POST'])
+def edit():
+    listofall = todo.get_all()
+    tododict = [t.to_dict() for t in listofall]
+    if request.method == 'GET':
+        return render_template('edit.html', todoid = tododict)
+    else:
+        id = int(request.form['id'])
+        title = request.form['title']
+        content = request.form['content']
+        status = bool(request.form.get('status'))
+        todo.update(title, content, status, id)
+        return redirect(url_for('action.index'))
